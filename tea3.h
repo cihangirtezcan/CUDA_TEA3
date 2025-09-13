2e342a36a92d77a57a1548a9f462a963350358d0
Cihangir Tezcan <71648358+cihangirtezcan@users.noreply.github.com>
2025-09-13 13:05:59 -0700

    Delete tea3.h

--- tea3.h
+++
@@ -1,8 +0,0 @@
-#ifndef HAVE_TEA3_H
-#define HAVE_TEA3_H
-
-#include <inttypes.h>
-
-void tea3(uint32_t dwFrameNumbers, uint8_t *lpKey, uint32_t dwNumKsBytes, uint8_t *lpKsOut);
-
-#endif /* HAVE_TEA3_H */
